a05887e8485dea3889159eff14006de999d0e567
jsanche7 <80990132+jsanche7@users.noreply.github.com>
2022-02-21 20:07:44 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,65 @@
-radio.onReceivedString(function (receivedString) {
-	
+function eskinterm2 () {
+    DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBR, Color.RED)
+    basic.pause(200)
+    DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBR, Color.OFF)
+    basic.pause(200)
+}
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("eskint")
 })
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("klaxona")
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "eskuina") {
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 30)
+        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 150)
+    }
+    if (receivedString == "ezkerra") {
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 150)
+        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 30)
+    }
+    if (receivedString == "aurrera") {
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 170)
+    }
+    if (receivedString == "atzera") {
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, 150)
+    }
+    if (receivedString == "ezkint") {
+        eskinterm()
+    }
+    if (receivedString == "eskint") {
+        eskinterm2()
+    }
+    if (receivedString == "klaxona") {
+        music.playMelody("C E G C E G - - ", 468)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("ezkint")
+})
+function eskinterm () {
+    DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBL, Color.RED)
+    basic.pause(200)
+    DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBL, Color.OFF)
+    basic.pause(200)
+}
 radio.setGroup(1)
 basic.forever(function () {
     if (input.acceleration(Dimension.X) > 200) {
         radio.sendString("eskuina")
+        basic.showArrow(ArrowNames.East)
     }
     if (input.acceleration(Dimension.X) < -200) {
         radio.sendString("ezkerra")
-    }
-    if (input.acceleration(Dimension.Y) > 200) {
-        radio.sendString("aurrera")
+        basic.showArrow(ArrowNames.West)
     }
     if (input.acceleration(Dimension.Y) < -200) {
+        radio.sendString("aurrera")
+        basic.showArrow(ArrowNames.North)
+    }
+    if (input.acceleration(Dimension.Y) > 200) {
         radio.sendString("atzera")
+        basic.showArrow(ArrowNames.South)
     }
 })
